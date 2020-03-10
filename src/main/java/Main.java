3537c011f289cbf1293bb68f11f9a10d401197d2
DobosP <pdobos@yahoo.om>
2020-03-10 19:22:02 +0200

    ai adaugat bine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Repository<Long, Book> bookRepository = new BookFileRepository("D:\\MPP-Laboratory-master\\src\\main\\java\\data\\books.txt");
         Repository<Long, Purchase> purchaseRepository = new InMemoryRepository<>();
         Validator<Purchase> purchaseValidator = new PurchaseValidator(bookRepository, clientRepository);
-        clientRepository.save(new Client(Long.valueOf(1),"Paul"));
+        clientRepository.save(new Client(Long.valueOf(1),"Paul Dobos"));
         clientRepository.save(new Client(Long.valueOf(2),"Tudor"));
         purchaseRepository.save(new Purchase(Long.valueOf(1),Long.valueOf(2),Long.valueOf(1)));
         ClientService clientService = new ClientService(clientRepository, clientValidator);
